037840017a2d01f8e0da8ecd386e9ce75e91b7f2
Arya kesarwani <aryakesharwani1234@gmail.com>
2025-11-04 15:40:56 +0530

    feat: add Somnia Testnet validators configuration and utility functions for retrieval

---
+++ backend/src/config/somniaValidators.js
@@ -0,0 +1,315 @@
+/**
+ * Somnia Testnet Validators
+ * 
+ * List of all validators on Somnia testnet with their addresses
+ */
+
+export const SOMNIA_VALIDATORS = [
+  {
+    name: "Allnodes",
+    address: "0xcFC7e08c00764A28bDfC02c34A65Ac22D4Cbfeb4",
+  },
+  {
+    name: "Cosmostation",
+    address: "0xD9A8EC483041e18376E16B16590B42544bBc9160",
+  },
+  {
+    name: "B-Harvest",
+    address: "0x2aEeFB7D9dC9345d759EebE6183888006723a2e9",
+  },
+  {
+    name: "dTeam",
+    address: "0x8B96C17d988fE1e36BecD0343Cc15b2a7E5B2de4",
+  },
+  {
+    name: "Nodemonster",
+    address: "0xDc24008fC384B2bD9759725e4Faa3F5b4010573d",
+  },
+  {
+    name: "Simplystaking",
+    address: "0x8CaA4E607c6c2AE7b345014a0E9E084dC57B4FBa",
+  },
+  {
+    name: "Flipside",
+    address: "0x3B14dd6F1819768b7C56c8eE8BF7a5f55D411d5f",
+  },
+  {
+    name: "Tranchess",
+    address: "0xa904F8B8E2455E00D8a3c03c9064DB3f1130bb65",
+  },
+  {
+    name: "GlobalStake",
+    address: "0x980A703CC531A3d8e5A17B843Dbc4645008915c6",
+  },
+  {
+    name: "0xAf43...840F",
+    address: "0xAf43e6f892ba3D9fE110f78568ecbF68250C840F",
+  },
+  {
+    name: "Nansen",
+    address: "0x1c4cf549eB8E3DDC98A304197D38DF7668F2bFf6",
+  },
+  {
+    name: "0xfBc4...6932",
+    address: "0xfBc4130948FeC3b4803260BEe0eb211cfD7C6932",
+  },
+  {
+    name: "0x4D21...8CDC",
+    address: "0x4D21b817f553fFE49CDdDca4aa9D9abF11178CDC",
+  },
+  {
+    name: "0xBE64...7FF2",
+    address: "0xBE64CA1Bff221b457f43e300D628D66A23fC7FF2",
+  },
+  {
+    name: "0x0dD2...01dC",
+    address: "0x0dD20c79fe2959F16c651d8e09E624987C9701dC",
+  },
+  {
+    name: "OriginStake",
+    address: "0x3f2C2d7ec0e3711393B7Fd73Da4D3F7B8b35E79A",
+  },
+  {
+    name: "Hightower",
+    address: "0x8cEF30E5abD81Ff9490BF490547B7f29373A3614",
+  },
+  {
+    name: "Iskra",
+    address: "0x4876bEc04d07942e0f56248C184c9F877169FA40",
+  },
+  {
+    name: "Hacken",
+    address: "0xd5525F27c2e268CD04197E29EbEede47608155cd",
+  },
+  {
+    name: "ValiDAO",
+    address: "0x9B3c29eE339bC0f7b7AE3638d39aAC8B014B1bB2",
+  },
+  {
+    name: "Crouton Digital",
+    address: "0x36Acef0aE66D69b0a9FEFC23ECA23F29486FeC88",
+  },
+  {
+    name: "Moonlet",
+    address: "0xa2509bf8825233Ebf05d6668E16A8E841D4B8F57",
+  },
+  {
+    name: "0x54f8...9557",
+    address: "0x54f8a9012dAC90Dc2314bB1d00f2d6A73AEF9557",
+  },
+  {
+    name: "0xe09A...1412",
+    address: "0xe09A2F3F0C8ec53Debc1894281e3121507731412",
+  },
+  {
+    name: "PierTwo",
+    address: "0x92103f82AfC86D5ac86A06b8E4A5001467FbFad5",
+  },
+  {
+    name: "Helios",
+    address: "0x33a00962758505387BF72A0567FaC6E467F239B6",
+  },
+  {
+    name: "Swyke",
+    address: "0xE4026557868CD2e43758F0596Cb55a37D7F728DE",
+  },
+  {
+    name: "LayerHub",
+    address: "0x16c38c1b3D6f8d07c6259De1550C1aF7db9BAE0A",
+  },
+  {
+    name: "Artifact",
+    address: "0x56c78615aEce02a8e7F5Ded9403027F15c32e404",
+  },
+  {
+    name: "0xd650...bCeC",
+    address: "0xd65042949A6EE19805fA0B9Afb42A28CA3c4bCeC",
+  },
+  {
+    name: "Enigma",
+    address: "0xd247147a9678Db94Cbb6c189CB6365DaB3349839",
+  },
+  {
+    name: "Xangle",
+    address: "0xA478B24fb0D5d2B57696E3B6F1725025F006a038",
+  },
+  {
+    name: "Validator Republic",
+    address: "0x30787D0F7240aD5ea8Cc1181D63b86E9C9E9838E",
+  },
+  {
+    name: "DSRV",
+    address: "0xB74C4bfccCF1B06E58c411F0C3Eb15Ac484c8a88",
+  },
+  {
+    name: "0x842a...EE31",
+    address: "0x842a16684E4f9b9362D8853D419615E8596BEE31",
+  },
+  {
+    name: "tttVn",
+    address: "0x6e22Af0b819d48B1fB6c5114Bb6Bc7e5Bf95A0BB",
+  },
+  {
+    name: "Somnia 2",
+    address: "0x8CF8fD3DE8640254dFfaD0B117eaec68d89566C6",
+  },
+  {
+    name: "Somnia 3",
+    address: "0xDaCb89974980ae907e5fd39D78F3376178EE811A",
+  },
+  {
+    name: "Somnia Seed Peer",
+    address: "0xc700201f3a32e1f69d768FE324e95c9D0eA821b0",
+  },
+  {
+    name: "Coinage-DAIC",
+    address: "0xC168ccB2544c053C2a221d0eDC59bc30a31BC859",
+  },
+  {
+    name: "BCW",
+    address: "0x3A7efB6ba58c5fE5a344fb27402CE1ca1572a870",
+  },
+  {
+    name: "Somnia 0",
+    address: "0xEaedE1c36f665258ce04820a759C9eBCc4e7e5D7",
+  },
+  {
+    name: "0xCe05...98Cc",
+    address: "0xCe05aC7A50767FbD533414AD78C42E87F91f98Cc",
+  },
+  {
+    name: "Infstones",
+    address: "0xD2f97112305bc61816bb0172F37d50889645b3c2",
+  },
+  {
+    name: "0x902c...FF1F",
+    address: "0x902c82753094C613Bf33ab1D23027ab8184dFF1F",
+  },
+  {
+    name: "SNZ",
+    address: "0xC435e9a7fd3C0AA40f5AeBAB284995510A1E716b",
+  },
+  {
+    name: "KingNodes",
+    address: "0x505B0f127247eC497Bc7Df6beC32ebE2c1Be655D",
+  },
+  {
+    name: "Luganodes",
+    address: "0xA6473f0Dd45C530f7aa397c66Cb495b57a88b999",
+  },
+  {
+    name: "Encapsulate",
+    address: "0xffA5B629CB85011284bB7708A3E4Fe3759956021",
+  },
+  {
+    name: "Meria",
+    address: "0x391efA3Eb47BD8D9e4a4A4A9eE92CA55C4B2C4FD",
+  },
+  {
+    name: "0x2c08...A7b6",
+    address: "0x2c08409ea72136B6D02c2A1d3d56Dfb74F44A7b6",
+  },
+  {
+    name: "Nodestake",
+    address: "0x17686EA673172D976c3e2456a2b533Dd68588493",
+  },
+  {
+    name: "ValidatorVn",
+    address: "0x0E57e18196aA092B374B0B8c74027CcaC01CD9ff",
+  },
+  {
+    name: "0xbe88...c7BA",
+    address: "0xbe88a1bFd89A2Df0d8A951B434ec560Ae3eFc7BA",
+  },
+  {
+    name: "Everstake",
+    address: "0x84df41070B72Fb96Fc6D20a948ffE5c0FAF4740B",
+  },
+  {
+    name: "Nethermind",
+    address: "0xDfE7E95c2E321464379A0649eff617ABCd53d620",
+  },
+  {
+    name: "Stakin",
+    address: "0x0B5ceb82Fe8c2c4Ee86dB4372C01a78F7DF03F93",
+  },
+  {
+    name: "Rubynodes",
+    address: "0x6cd81e02Cff42850A3490d3e9D9C272BAaB754d6",
+  },
+  {
+    name: "Imperator",
+    address: "0x77E0A78Ce13409FEB038A3D590baE93130A35d7e",
+  },
+  {
+    name: "Technodes",
+    address: "0x8B1997fb66F6A3fb8AF1059439dE94D30De2c161",
+  },
+  {
+    name: "01node",
+    address: "0x87B62349E6Cc06191e084c587e3042E283054c61",
+  },
+  {
+    name: "ValidationCloud",
+    address: "0x34A4C5D00dD4dB19B2400E37c4a3B4D76301fE1b",
+  },
+  {
+    name: "P-OPS",
+    address: "0x329E9BDE7f64DAB3CadA2652D1025a6f53272F01",
+  },
+  {
+    name: "Nodesguru",
+    address: "0xaE64E22Dc9F8Fe58C2F9d2a6F07De091468AF982",
+  },
+  {
+    name: "Stakeme",
+    address: "0xE8E4d93aa612a79d87a8A89Af827136d5CBD8F59",
+  },
+  {
+    name: "Block Hunters",
+    address: "0x18a7276A6eE2610DA0Ce95C279C88f9D9249162e",
+  },
+  {
+    name: "Chainbase",
+    address: "0x96E2cc08a49fec32773F79E263f5F19D06637c87",
+  },
+];
+
+/**
+ * Get validator by address
+ * @param {string} address - Validator address
+ * @returns {Object|null} Validator object or null if not found
+ */
+export function getValidatorByAddress(address) {
+  return SOMNIA_VALIDATORS.find(
+    (v) => v.address.toLowerCase() === address.toLowerCase()
+  ) || null;
+}
+
+/**
+ * Get validator by name
+ * @param {string} name - Validator name
+ * @returns {Object|null} Validator object or null if not found
+ */
+export function getValidatorByName(name) {
+  return SOMNIA_VALIDATORS.find(
+    (v) => v.name.toLowerCase() === name.toLowerCase()
+  ) || null;
+}
+
+/**
+ * Get all validator addresses
+ * @returns {string[]} Array of validator addresses
+ */
+export function getAllValidatorAddresses() {
+  return SOMNIA_VALIDATORS.map((v) => v.address);
+}
+
+/**
+ * Get all validators
+ * @returns {Array} Array of all validators
+ */
+export function getAllValidators() {
+  return SOMNIA_VALIDATORS;
+}
+
